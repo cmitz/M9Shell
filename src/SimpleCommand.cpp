6440f326a39ab6df7099876c1a88d151eca8b905
Casper Smits <casper@caspersmits.nl>
2019-05-15 22:20:59 +0200

    Add pwd command

--- src/SimpleCommand.cpp
+++ src/SimpleCommand.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include <unistd.h>
 #include "SimpleCommand.h"
 
 void SimpleCommand::execute() {
-	std::cout << "FIXME: You should change SimpleCommand::execute()" << std::endl;
-	// FIXME: Your code here...
+//    std::string              command;
+//    std::vector<std::string> arguments;
+//    std::vector<IORedirect>  redirects;
+
+    // Built-in command PWD that tells us the current working directory
+    if (command == "pwd") {
+        char cwd[256];
+        if (getcwd(cwd, sizeof(cwd)) == nullptr)
+            std::cerr << "getcwd() error" << std::endl;
+        else
+            std::cout << cwd << std::endl;
+    }
 }
